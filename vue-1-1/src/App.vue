d0e4aff5920bcbb63dff9f4a2a07f6a5fe035cc9
Alex Ratushnyi <alexander.ratushnyi@gmail.com>
2020-06-08 19:55:06 +0300

    ✨Add vue lesson

---
+++ vue-1-1/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app" class="app">
+    <h1 class="app__title">{{ getLoadingState }}</h1>
+    <p class="app__text">Email: {{ email }}</p>
+    <p class="app__text">Password: {{ password }}</p>
+    <input class="app__input" type="email" v-model='email'>
+    <input class="app__input" type="password" v-model='password'>
+    <app-button @click="send" class="app__button">Button 1</app-button>
+    <app-button @click="toggleLoading" :loading="loading" class="app__button">Button 2</app-button>
+  </div>
+</template>
+
+<script>
+import AppButton from "@/components/common/AppButton.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppButton
+  },
+  data() {
+    return {
+      loading: false,
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    send() {
+      alert(`Email: ${this.email}; Password: ${this.password}`);
+    },
+    toggleLoading() {
+      this.loading = !this.loading;
+    }
+  },
+  computed: {
+    getLoadingState() {
+      return this.loading ? 'Loading...' : 'Hello World!'
+    }
+  }
+};
+</script>
+
+<style lang='scss'>
+$style: app;
+.#{$style} {
+  padding: 40px;
+  @include flex-col(flex-start, center);
+  &__title {
+    color: $dark-color;
+  }
+  &__text {
+    margin: 12px 0;
+  }
+  &__input {
+    margin-bottom: 24px;
+  }
+  &__button {
+    &:not(:last-of-type) {
+      margin-bottom: 24px;
+    }
+  }
+}
+</style>
---
+++ vue-1-1/src/components/common/AppButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button @click="() => $emit('click')" class="button" :class="{ _loading: loading }">
+    <template v-if="loading">Loading...</template>
+    <slot v-else />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AppButton",
+  props: {
+    loading: Boolean
+  }
+};
+</script>
+
+<style lang="scss">
+$style: button;
+.#{$style} {
+  padding: 12px 32px;
+  border-radius: 4px;
+  background-color: $button-color;
+  color: white;
+  @include flex(center, center);
+  cursor: pointer;
+  @include transition(all);
+  &:hover {
+    background-color: lighten($button-color, 10%);
+  }
+  &:active {
+    background-color: darken($button-color, 10%);
+  }
+  &._loading {
+    background-color: blueviolet;
+  }
+}
+</style>
